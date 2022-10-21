2a18f85497fd56095c0f70ac3677cbc523169156
Prince Mohammed Huzaifa <prince.mohammed@amalitech.org>
2022-10-21 15:39:30 +0000

    Implemented the all the components of the game and game logic.

---
+++ js/main.js
@@ -0,0 +1,63 @@
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * choices.length);
+    return choices[randomNumber];
+}
+
+function choose(choice) {
+    const computerChoice = getComputerChoice();
+    displayResults([choice, computerChoice]);
+    displayWinner([choice, computerChoice]);
+}
+
+function displayResults(results) {
+    resultDivs.forEach((resultDiv, index) => {
+        setTimeout(() => {
+            resultDiv.innerHTML = `
+                <div class="choice ${results[index]} d-grid place-items-center">
+                    <img src="./images/icon-${results[index]}.svg" alt="results[index]">
+                </div>
+
+            `;
+        }, index * 1000);
+    });
+
+    gameDiv.classList.toggle("display-none");
+    resultsDiv.classList.toggle("display-none");
+}
+
+function displayWinner(results) {
+    setTimeout(() => {
+        const verdict = determineWinner(results);
+
+        if (verdict === "win") {
+            resultText.innerText = "You win";
+            resultDivs[0].classList.toggle("winner");
+        } else if (verdict === "loose") {
+            resultText.innerText = "You loose";
+            resultDivs[1].classList.toggle("winner");
+        } else {
+            resultText.innerText = "Draw";
+        }
+
+        resultWinner.classList.toggle("display-none");
+        resultsDiv.classList.toggle("show-winner");
+    }, 1000);
+}
+
+function determineWinner(results) {
+    if (
+        (results[0] === "rock" && results[1] === "scissors") ||
+        (results[0] === "paper" && results[1] === "rock") ||
+        (results[0] === "scissors" && results[1] === "paper")
+    ) {
+        return "win";
+    } else if (
+        (results[1] === "rock" && results[0] === "scissors") ||
+        (results[1] === "paper" && results[0] === "rock") ||
+        (results[1] === "scissors" && results[0] === "paper")
+    ) {
+        return "loose";
+    } else {
+        return "draw";
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,52 @@
+const rules = document.querySelector(".rules-btn");
+const modal = document.querySelector(".modal");
+const closeBtn = document.querySelector(".close-btn");
+
+const choiceBtns = document.querySelectorAll(".choice-btn");
+const gameDiv = document.querySelector(".game");
+const resultsDiv = document.querySelector(".results");
+const resultDivs = document.querySelectorAll(".results__result");
+
+const resultWinner = document.querySelector(".results__winner");
+const resultText = document.querySelector(".results__text");
+
+const choices = ["rock", "paper", "scissors"];
+
+rules.addEventListener("click", (e) => {
+    modal.classList.toggle("hide");
+});
+
+closeBtn.addEventListener("click", (e) => {
+    modal.classList.toggle("hide");
+});
+
+// GAME LOGIC
+choiceBtns.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        const choiceName = button.dataset.choice;
+        choose(choiceName);
+    });
+});
+
+// function getComputerChoice() {
+//     const randomNumber = Math.floor(Math.random() * choices.length);
+//     return choices[randomNumber];
+// }
+
+// function choose(choice) {
+//     const computerChoice = getComputerChoice();
+//     displayResults([choice, computerChoice]);
+// }
+
+// function displayResults(results) {
+//     resultDivs.forEach((resultDiv, index) => {
+//         setTimeout(() => {
+//             resultDiv.innerHTML = `
+//                 <div class="choice ${results[index]} d-grid place-items-center">
+//                     <img src="./images/icon-${results[index]}.svg" alt="results[index]">
+//                 </div>
+
+//             `;
+//         }, index * 1000);
+//     });
+// }
